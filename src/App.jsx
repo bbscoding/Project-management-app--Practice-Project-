4053f77ca73a97ffbe8f8d58a08a3a47b129ca5b
bbscoding <batuhan5534@gmail.com>
2025-12-25 13:11:38 +0300

    Collect User Input With Ref & Forwarded Ref

--- src/App.jsx
+++ src/App.jsx
@@ -17,16 +17,32 @@ function App() {
       }
     });
   }
+
+  function handleAddProject(projectData) {
+    setProjectsState(prevState => {
+      const newProject = {
+        ...projectData,
+        id: Math.random()
+      }
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject]
+      }
+    })
+  }
+
+  console.log(projectsState)
   let content;
+
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject />
+    content = <NewProject onAdd={handleAddProject} />
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
   }
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar onStartAddProject={handleStartAddProject} />
-      <NoProjectSelected onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
